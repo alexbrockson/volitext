587cb0dd6b29d05d9bdb793c637c80622e5852ef
alexbrockson <alexbrockson@gmail.com>
2023-12-09 16:36:14 -0600

    Theme, project first steps

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,20 +1,17 @@
 <script>
 	import { page } from '$app/stores';
-	import logo from '$lib/images/svelte-logo.svg';
+	// import logo from '$lib/images/svelte-logo.svg';
 	import github from '$lib/images/github.svg';
 </script>
 
 <header>
 	<div class="corner">
-		<a href="https://kit.svelte.dev">
+		<!-- <a href="https://kit.svelte.dev">
 			<img src={logo} alt="SvelteKit" />
-		</a>
+		</a> -->
 	</div>
 
 	<nav>
-		<svg viewBox="0 0 2 3" aria-hidden="true">
-			<path d="M0,0 L1,2 C1.5,3 1.5,3 2,3 L2,0 Z" />
-		</svg>
 		<ul>
 			<li aria-current={$page.url.pathname === '/' ? 'page' : undefined}>
 				<a href="/">Home</a>
@@ -22,13 +19,10 @@
 			<li aria-current={$page.url.pathname === '/about' ? 'page' : undefined}>
 				<a href="/about">About</a>
 			</li>
-			<li aria-current={$page.url.pathname.startsWith('/sverdle') ? 'page' : undefined}>
+			<!-- <li aria-current={$page.url.pathname.startsWith('/sverdle') ? 'page' : undefined}>
 				<a href="/sverdle">Sverdle</a>
-			</li>
+			</li> -->
 		</ul>
-		<svg viewBox="0 0 2 3" aria-hidden="true">
-			<path d="M0,0 L0,3 C0.5,3 0.5,3 1,2 L2,0 Z" />
-		</svg>
 	</nav>
 
 	<div class="corner">
@@ -67,16 +61,6 @@
 		display: flex;
 		justify-content: center;
 		--background: rgba(255, 255, 255, 0.7);
-	}
-
-	svg {
-		width: 2em;
-		height: 3em;
-		display: block;
-	}
-
-	path {
-		fill: var(--background);
 	}
 
 	ul {
---
+++ src/routes/openroom/+page.svelte
@@ -0,0 +1,50 @@
+<script lang="ts">
+	// export let isOpen = false;
+
+	function go() {
+		alert("you clicked the button");
+	}
+</script>
+
+<div>
+	<!-- <label for="txtRoomName">Enter a room name:</label>
+	<input id="txtRoomName" class="textbox"  /> -->
+
+
+	<form method="POST">
+		<label>
+			Room name:
+			<input name="room" type="text">
+		</label>
+		<label>
+			Password:
+			<input name="password" type="password">
+		</label>
+		<button id="btnGo" on:click={go}>Go</button>
+	</form>
+</div>
+<!-- {#if isOpen}
+<div class="modal">
+    This is a modal
+    <button on:click={closeModal} class="button">Close Modal</button>
+</div>
+{/if} -->
+
+<!-- <button on:click={() => (count -= 1)} aria-label="Decrease the counter by one">
+		<svg aria-hidden="true" viewBox="0 0 1 1">
+			<path d="M0,0.5 L1,0.5" />
+		</svg>
+	</button>
+
+	<div class="counter-viewport">
+		<div class="counter-digits" style="transform: translate(0, {100 * offset}%)">
+			<strong class="hidden" aria-hidden="true">{Math.floor($displayed_count + 1)}</strong>
+			<strong>{Math.floor($displayed_count)}</strong>
+		</div>
+	</div>
+
+	<button on:click={() => (count += 1)} aria-label="Increase the counter by one">
+		<svg aria-hidden="true" viewBox="0 0 1 1">
+			<path d="M0,0.5 L1,0.5 M0.5,0 L0.5,1" />
+		</svg>
+	</button> -->
